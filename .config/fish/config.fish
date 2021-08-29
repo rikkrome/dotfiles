9b1175945496c6587787cde0369a9f222f4fe425
ricky romero <contact@romeroricky.com>
2021-08-29 13:29:36 -0700

    updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,19 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+set fish_greeting ""
 
+
+if type -q exa
+  alias ll "exa -l -g"
+  alias lla "ll -a"
+end
+
+set -x FZF_DEFAULT_COMMAND 'rg --files --follow --hidden'
+
+# React native 
+set -x ANDROID_HOME $HOME/Library/Android/sdk
+set -x PATH $PATH $ANDROID_HOME/emulator
+set -x PATH $PATH $ANDROID_HOME/tools
+set -x PATH $PATH $ANDROID_HOME/tools/bin
+set -x PATH $PATH: ANDROID_HOME/platform-tools
