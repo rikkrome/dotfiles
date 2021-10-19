b8ed3741c4acde1a4fc3305baa857d5a75d4dbf6
Ricky Romero <contact@romeroricky.com>
2021-10-19 14:04:31 -0700

    updated fish config - android

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,4 +17,4 @@ set -x ANDROID_HOME $HOME/Library/Android/sdk
 set -x PATH $PATH $ANDROID_HOME/emulator
 set -x PATH $PATH $ANDROID_HOME/tools
 set -x PATH $PATH $ANDROID_HOME/tools/bin
-set -x PATH $PATH: ANDROID_HOME/platform-tools
+set -x PATH $PATH $ANDROID_HOME/platform-tools
