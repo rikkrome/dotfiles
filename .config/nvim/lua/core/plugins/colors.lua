8de1e42896c350a28137a34ad268896a75d954e3
ricky romero <contact@romeroricky.com>
2024-03-08 17:45:36 -0800

    - nvim update colors

--- .config/nvim/lua/core/plugins/colors.lua
+++ .config/nvim/lua/core/plugins/colors.lua
@@ -1,12 +1,3 @@
-function Colors(color)
-  color = color or "rose-pine"
-  vim.cmd.colorscheme(color)
-
-  -- remove theme BG color
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
 return {
   {
     "folke/tokyonight.nvim",
@@ -14,7 +5,7 @@ return {
       require("tokyonight").setup({
         -- your configuration comes here
         -- or leave it empty to use the default settings
-        style = "storm",        -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        style = "night",        -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
         transparent = true,     -- Enable this to disable setting the background color
         terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
         styles = {
@@ -23,21 +14,67 @@ return {
           comments = { italic = false },
           keywords = { italic = false },
           -- Background styles. Can be "dark", "transparent" or "normal"
-          sidebars = "dark", -- style for sidebars, see below
-          floats = "dark",   -- style for floating windows
+          -- sidebars = "dark", -- style for sidebars, see below
+          -- floats = "dark",   -- style for floating windows
+          sidebars = "transparent",
+          floats = "transparent"
         },
+        dim_inactive = true,
+        on_highlights = function(hl, c)
+          hl.CursorLine = {
+            bg = c.bg_highlight,
+          }
+          hl.CursorLineNr = {
+            fg = c.blue,
+          }
+          hl.LineNr = {
+            fg = c.fg_dark
+          }
+          -- gitSigns
+          hl.GitSignsAdd = {
+            fg = "#fa9500",
+          }
+          hl.GitSignsChange = {
+            fg = "#FFE45E"
+          }
+          hl.GitSignsDelete = {
+            fg = "#EB6424"
+          }
+          -- Telescope
+          hl.TelescopeNormal = {
+            bg = "#000000",
+            fg = "#ffffff",
+          }
+          hl.TelescopeBorder = {
+            bg = "#000000",
+            fg = "#ffffff",
+          }
+          hl.TelescopePromptNormal = {
+            bg = "#000000",
+            fg = "#ffffff",
+          }
+          hl.TelescopePromptBorder = {
+            bg = "#000000",
+            fg = "#ffffff",
+          }
+          hl.TelescopePromptTitle = {
+            bg = "#000000",
+            fg = "#ffffff",
+          }
+          hl.TelescopePreviewTitle = {
+            bg = "#000000",
+            fg = "#ffffff",
+          }
+          hl.TelescopeResultsTitle = {
+            bg = "#000000",
+            fg = "#ffffff",
+          }
+        end,
       })
+      vim.cmd [[colorscheme tokyonight]]
+      -- remove theme BG color
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end
   },
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    config = function()
-      require("rose-pine").setup({
-        disable_background = true
-      })
-      vim.cmd('colorscheme rose-pine')
-      Colors()
-    end
-  }
 }
